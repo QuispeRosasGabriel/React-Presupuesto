4cac789aea2f45f746c57d058bb51668796f38fa
Gabriel Quispe Rosas <gabrielomarquisperosas@gmail.com>
2020-04-21 11:15:13 -0500

    working on form

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ function App() {
   const [presupuesto, guardarPresupuesto] = useState(0);
   const [restante, guardarRestante] = useState(0);
   const [mostrarPregunta, actualizarPregunta] = useState(true);
+  const [gastos, guardarGastos] = useState([]);
+
+  //Cuando agreguemos nuevo gasto
+  const agregarNuevoGasto = (gasto) => {
+    guardarGastos([...gastos, gasto]);
+  };
 
   return (
     <Fragment>
@@ -24,7 +30,7 @@ function App() {
             ) : (
               <div className="row">
                 <div className="one-half column">
-                  <Formulario />
+                  <Formulario agregarNuevoGasto={agregarNuevoGasto} />
                 </div>
                 <div className="one-half column">2</div>
               </div>
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,18 +1,56 @@
 import React, { useState } from "react";
+import Modal from "./Modal";
+import shortid from "shortid";
 
-const Formulario = () => {
+const Formulario = ({ agregarNuevoGasto }) => {
+  const [nombre, guardarNombre] = useState("");
+  const [cantidad, guardarCantidad] = useState(0);
+  const [error, guardarError] = useState(false);
+
+  const agregarGasto = (e) => {
+    e.preventDefault();
+    //validar
+    if (cantidad < 1 || isNaN(cantidad) || nombre.trim() === "") {
+      guardarError(true);
+      return;
+    }
+    guardarError(false);
+    //construir el gasto
+    const gasto = {
+      nombre,
+      cantidad,
+      id: shortid.generate(),
+    };
+
+    //pasar el gasto principal
+    agregarNuevoGasto(gasto);
+
+    //resetear el form
+    guardarNombre("");
+    guardarCantidad(0);
+  };
+
   return (
-    <form action="">
+    <form action="" onSubmit={agregarGasto}>
       <h2>Agregar tus gastos aquí</h2>
+      {error && <Modal mensaje="Complete todos los campos del gasto" />}
       <div className="campo">
         <label htmlFor="">Nombre del gasto</label>
         <input
           type="text"
           className="u-full-width"
           placeholder="Ej. Transporte"
+          value={nombre}
+          onChange={(e) => guardarNombre(e.target.value)}
         />
         <label htmlFor="">Cantidad del gasto</label>
-        <input type="number" className="u-full-width" placeholder="Ej. 300" />
+        <input
+          type="number"
+          className="u-full-width"
+          value={cantidad}
+          onChange={(e) => guardarCantidad(parseInt(e.target.value))}
+          placeholder="Ej. 300"
+        />
         <input
           type="submit"
           className="button-primary u-full-width"
